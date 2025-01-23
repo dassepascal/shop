9e82cceeabb460199e74ee00c4fbbb443f8f9aea
Dasse <pascaldasse56@gmail.com>
2025-01-23 10:05:17 +0100

    add relation order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,7 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Payment;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
@@ -12,4 +16,29 @@ class Order extends Model
     protected $fillable = [
         'shipping', 'tax', 'user_id', 'state_id', 'payment', 'reference', 'pick', 'total',
     ];
+
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(OrderAddress::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(OrderProduct::class);
+    }
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function payment_infos(): HasOne
+    {
+        return $this->hasOne(Payment::class);
+    }
 }
