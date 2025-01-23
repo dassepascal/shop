cf4d554196cae37d185fc667201a7469dbe4b0fb
Dasse <pascaldasse56@gmail.com>
2025-01-23 13:48:42 +0100

    mdp forget

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,6 @@ Volt::route('/pages/{page:slug}', 'page')->name('pages');
 Route::middleware('guest')->group(function () {
 	Volt::route('/register', 'auth.register');
 	Volt::route('/login', 'auth.login')->name('login');
+	Volt::route('/forgot-password', 'auth.forgot-password');
+	Volt::route('/reset-password/{token}', 'auth.reset-password')->name('password.reset');
 });
