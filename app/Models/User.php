ef40c75f3c51169720c4bf999044578aa9bf5c9e
Dasse <pascaldasse56@gmail.com>
2025-01-23 10:04:08 +0100

    addd relation users

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -43,4 +44,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function addresses(): HasMany
+{
+    return $this->hasMany(Address::class);
+}
+
+public function orders(): HasMany
+{
+    return $this->hasMany(Order::class);
+}
 }
