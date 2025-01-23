bc0e0ae17301f58555eac351e75bd617e471e188
Dasse <pascaldasse56@gmail.com>
2025-01-23 09:42:14 +0100

    address

---
+++ app/Models/Address.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'firstname',
+        'professionnal',
+        'civility',
+        'company',
+        'address',
+        'addressbis',
+        'bp',
+        'postal',
+        'city',
+        'phone',
+        'country_id',
+    ];
+}
