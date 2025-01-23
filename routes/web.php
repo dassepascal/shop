def79cad43b2b5bf46bc4b548ca50afba436ce36
Dasse <pascaldasse56@gmail.com>
2025-01-23 13:41:41 +0100

    login

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,5 @@ Volt::route('/pages/{page:slug}', 'page')->name('pages');
 
 Route::middleware('guest')->group(function () {
 	Volt::route('/register', 'auth.register');
+	Volt::route('/login', 'auth.login')->name('login');
 });
