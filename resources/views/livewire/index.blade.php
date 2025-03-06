0c20549a174ad908989e0e649d7a36624d1ccf71
Dasse <pascaldasse56@gmail.com>
2025-03-06 10:27:40 +0100

    add index à jour pour le prix promotion hors date

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -3,70 +3,58 @@
 use Livewire\Volt\Component;
 use App\Models\Product;
 
-new class extends Component 
-{
+new class extends Component {
     public function with(): array
     {
         return [
             'products' => Product::whereActive(true)->get(),
         ];
     }
-    
 }; ?>
 
 <div class="container mx-auto">
     @if (session('registered'))
-    <x-alert 
-        title="{!! session('registered') !!}" 
-        icon="s-rocket-launch" 
-        class="mb-4 alert-info" 
-        dismissible 
-    />
-@endif
-    <x-card class="w-full shadow-md shadow-gray-500" shadow separator >
+        <x-alert title="{!! session('registered') !!}" icon="s-rocket-launch" class="mb-4 alert-info" dismissible />
+    @endif
+    <x-card class="w-full shadow-md shadow-gray-500" shadow separator>
         {!! $shop->home !!}
     </x-card>
     <br>
     <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         @foreach ($products as $product)
-        @php
-        $bestPrice = getBestPrice($product);
-        if($bestPrice === $product->price){
-            $titleContent = '<span class="line-through">' . number_format($product->price, 2, ',', ' ') . ' € TTC</span> <span class="text-red-500">' . number_format($bestPrice, 2, ',', ' ') . ' € TTC</span>';
-        }else {
-            $titleContent =
-                '<span class="line-through">' .
-                number_format($product->price, 2, ',', ' ') .
-                ' € TTC</span> <span class="text-red-500">' .
-                number_format($bestPrice, 2, ',', ' ') .
-                ' € TTC</span>';
-        }
-        
-    @endphp
-          <x-card
-          class="shadow-md transition duration-500 ease-in-out shadow-gray-500 hover:shadow-xl hover:shadow-gray-500 flex flex-col justify-between">
-              {!! $titleContent !!}<br>
-              <b>{!! $product->name !!}</b>
-              @unless ($product->quantity)
-              <br><span class="text-red-500">@lang('Product out of stock')</span>
-              @endunless
-  
-          @if ($product->image)
-              <x-slot:figure>
-                  @if ($product->quantity)
-                      <a href="{{ route('products.show', $product) }}">
-                  @endif
-                  <img src="{{ asset('storage/photos/' . $product->image) }}" alt="{!! $product->name !!}" />
-                  @if ($product->quantity)
-                      </a>
-                  @endif
-              </x-slot:figure>
-          @endif
-      </x-card>
+            @php
+            $bestPrice = getBestPrice($product);
+            $hasPromotion = $bestPrice < $product->price; // Vérifie si une promotion est active
+            if ($hasPromotion) {
+                $titleContent = '<span class="line-through">' . number_format($product->price, 2, ',', ' ') . ' € TTC</span> <span class="text-red-500">' . number_format($bestPrice, 2, ',', ' ') . ' € TTC</span>';
+            } else {
+                $titleContent = '<span>' . number_format($product->price, 2, ',', ' ') . ' € TTC</span>';
+            }
+            @endphp
+            <x-card class="shadow-md transition duration-500 ease-in-out shadow-gray-500 hover:shadow-xl hover:shadow-gray-500 flex flex-col justify-between">
+                <b>{!! $product->name !!} :</b>
+                {!! $titleContent !!}<br>
+               
+                @unless ($product->quantity)
+                    <br><span class="text-red-500">@lang('Product out of stock')</span>
+                @endunless
+    
+                @if ($product->image)
+                    <x-slot:figure>
+                        @if ($product->quantity)
+                            <a href="{{ route('products.show', $product) }}">
+                        @endif
+                        <img src="{{ asset('storage/photos/' . $product->image) }}" alt="{!! $product->name !!}" />
+                        @if ($product->quantity)
+                            </a>
+                        @endif
+                    </x-slot:figure>
+                @endif
+            </x-card>
         @endforeach
     </div>
     <br>
-    <x-card class="w-full shadow-md shadow-gray-500" shadow separator >
+    <x-card class="w-full shadow-md shadow-gray-500" shadow separator>
         <x-accordion wire:model="group" class="shadow-md shadow-gray-500">
             <x-collapse name="group1">
                 <x-slot:heading>{{ __('General informations') }}</x-slot:heading>
