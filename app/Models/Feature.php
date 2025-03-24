3358beeb0b9c649790f387670d7e0d4c1b74ad83
Dasse <pascaldasse56@gmail.com>
2025-03-24 21:29:55 +0100

    modif code  model product + feature

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -9,4 +9,16 @@ class Feature extends Model
     protected $fillable=[
         'name'
     ];
+
+    /**
+     * The products that belong to the Feature
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'feature_product')
+                    ->withPivot('value')
+                    ->withTimestamps();
+    }
 }
